494593d19de1285612561cbec75fb1ba0ad77597
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-09-29 16:02:25 +0800

    describe POST variable by shell varible

--- github_initial_deploy.sh
+++ github_initial_deploy.sh
@@ -8,7 +8,7 @@ PROJECT_NAME="test092901"
 #cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" https:/api.github.com/user/repos
 
 #curl -X POST [url] -d "name=hoge&age=20"
-cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST https://api.github.com/user/repos -d '{"name":"test092901"}'
+cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST https://api.github.com/user/repos -d '{"name":"'$PROJECT_NAME'"}'
 
 #DELETE repository
-#cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -X DELETE https://api.github.com/repos/$USER/$PROJECT_NAME
+cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -X DELETE https://api.github.com/repos/$USER/$PROJECT_NAME
