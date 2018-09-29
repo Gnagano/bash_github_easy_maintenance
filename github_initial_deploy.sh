0952efea08b13e3cc0c46a3073c2eb73b855b182
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-09-30 00:04:49 +0800

    KEY_ID 削除

--- github_initial_deploy.sh
+++ github_initial_deploy.sh
@@ -1,23 +1,19 @@
 #! /bin/sh
-USER="gano2018"
-ACCESS_TOKEN="$HOME/.github/github_token"
-PROJECT_NAME="test092901"
-GITHUB_API_URL="https://api.github.com"
-KEY_COMMENT="gen.nagano.cc20120701@gmail.com"
-KEY_TITLE="gano@iMac"
+. settings.conf
+
 
 github_curl (){
   cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' $@
 }
 
 # create repository
-github_curl -X POST $GITHUB_API_URL/user/repos -d '{"name":"'$PROJECT_NAME'","private":"true"}'
+#github_curl -X POST $GITHUB_API_URL/user/repos -d '{"name":"'$PROJECT_NAME'","private":"true"}'
 
 # delete repository
-# github_curl -X DELETE $GITHUB_API_URL/repos/$USER/$PROJECT_NAME
+github_curl -X DELETE $GITHUB_API_URL/repos/$USER/$PROJECT_NAME
 
 # generate ssh key
-github_keygen -C $KEY_COMMENT $PROJECT_NAME
-
-# deploy ssh key
-cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST $GITHUB_API_URL/repos/$USER/$PROJECT_NAME/keys -d '{ "title" : "'$KEY_TITLE'","key" : "'"$(cat $HOME/.ssh/github_keys/$PROJECT_NAME.pub)"'" }'
+# github_keygen -C $KEY_COMMENT $PROJECT_NAME
+#
+# # deploy ssh key
+# cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST $GITHUB_API_URL/repos/$USER/$PROJECT_NAME/keys -d '{ "title" : "'$KEY_TITLE'","key" : "'"$(cat $HOME/.ssh/github_keys/$PROJECT_NAME.pub)"'" }'
