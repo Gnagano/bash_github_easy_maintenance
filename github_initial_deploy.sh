f9df0138a72f134fae8e8ccd33bf60eb13989063
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-09-29 18:39:08 +0800

    add code_backup.sh

--- github_initial_deploy.sh
+++ github_initial_deploy.sh
@@ -2,10 +2,11 @@
 USER="gano2018"
 ACCESS_TOKEN="$HOME/.github/github_token"
 PROJECT_NAME="test092901"
+GITHUB_API_URL="https://api.github.com"
 
 github_curl (){
   cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' $@
 }
 
-github_curl -X POST https://api.github.com/user/repos -d '{"name":"'$PROJECT_NAME'"}'
-github_curl -X DELETE https://api.github.com/repos/$USER/$PROJECT_NAME
+github_curl -X POST $GITHUB_API_URL/user/repos -d '{"name":"'$PROJECT_NAME'"}'
+github_curl -X DELETE $GITHUB_API_URL/repos/$USER/$PROJECT_NAME
