2643763dc16cd1974bb282f708e440840ed274c9
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-09-29 15:42:02 +0800

    create repository via api

--- github_initial_deploy.sh
+++ github_initial_deploy.sh
@@ -2,6 +2,10 @@
 
 USER="gano2018"
 ACCESS_TOKEN="$HOME/.github/github_token"
+PROJECT_NAME="test092901"
 
 #curl https://api.github.com/users/$USER/repos
-cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" https:/api.github.com
+#cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" https:/api.github.com/user/repos
+
+#curl -X POST [url] -d "name=hoge&age=20"
+cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST https://api.github.com/user/repos -d '{"name":"test092901"}'
