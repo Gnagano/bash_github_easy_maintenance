c4bfe9b50dfad94836b642cc978a1543eb7648d5
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-09-29 16:11:55 +0800

    extract common curl procedure

--- github_initial_deploy.sh
+++ github_initial_deploy.sh
@@ -1,14 +1,11 @@
 #! /bin/sh
-
 USER="gano2018"
 ACCESS_TOKEN="$HOME/.github/github_token"
 PROJECT_NAME="test092901"
 
-#curl https://api.github.com/users/$USER/repos
-#cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" https:/api.github.com/user/repos
+github_curl (){
+  cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' $@
+}
 
-#curl -X POST [url] -d "name=hoge&age=20"
-cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST https://api.github.com/user/repos -d '{"name":"'$PROJECT_NAME'"}'
-
-#DELETE repository
-cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -X DELETE https://api.github.com/repos/$USER/$PROJECT_NAME
+github_curl -X POST https://api.github.com/user/repos -d '{"name":"'$PROJECT_NAME'"}'
+github_curl -X DELETE https://api.github.com/repos/$USER/$PROJECT_NAME
