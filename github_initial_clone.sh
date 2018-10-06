d3ba4c721d49464cfafd9deecc6c0c8ba5c9dab5
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-10-06 19:53:12 +0800

    add precondition on github_initial_clone

--- github_initial_clone.sh
+++ github_initial_clone.sh
@@ -1,29 +1,47 @@
 #! /bin/sh
-. settings.conf
+
+#for calling from symbolic link
+if [ -L $0 ]; then
+  COMMAND_DIR=$(dirname `readlink $0`)
+else
+  COMMAND_DIR="."
+fi
+. $COMMAND_DIR/settings.conf
 
 # Exit if $1 is exit
 if [ -z $1 ]; then
-  echo  "\nUsage: github_inital_clone [ project_name ]\n" 1>&2
+  echo  "\n[Error] The way to use command is not correct.\n Usage: github_inital_clone [ project_name ]\n" 1>&2
   exit 1
 fi
 
 PROJECT_NAME=$1
 
-# generate ssh key
-sh ./github_keygen.sh -C $KEY_COMMENT $PROJECT_NAME
-
-# Exit if key has already existed
-if [ $? != 0 ]; then
+# Exit if current directory include .git
+if [ -d `pwd`/.git ]; then
+  echo  "\n[Error] The current directory has been already git repository.\n" 1>&2
   exit 1
 fi
 
-# 2. deploy key
+# Exit if directory PROJECT_NAME is existed
+if [ -d `pwd`/$PROJECT_NAME ]; then
+  echo  "\n[Error] git repository has been already cloned.\n" 1>&2
+  exit 1
+fi
+
+# generate ssh key
+sh $COMMAND_DIR/github_keygen.sh -C $KEY_COMMENT $PROJECT_NAME
+
+# Exit if key has already existed
+#if [ $? != 0 ]; then
+#  exit 1
+#fi
 
 # deploy ssh key
 cat $ACCESS_TOKEN | xargs -I {} curl -H "Authorization: token {}" -H 'Content-Type:application/json' -X POST $GITHUB_API_URL/repos/$USER/$PROJECT_NAME/keys -d '{ "title" : "'$KEY_TITLE'","key" : "'"$(cat $HOME/.ssh/github_keys/$PROJECT_NAME.pub)"'" }'
 
 # 3. git remote origin add
-git remote add origin git@$PROJECT_NAME:$USER/$PROJECT_NAME.git
+git clone git@$PROJECT_NAME:$USER/$PROJECT_NAME.git
 
 # 4. git fetch origin
+cd $PROJECT_NAME
 git fetch origin
